89ecdd337e21b996c23c83239e944f1a1da5e452
정원철 <gksrud316@naver.com>
2023-01-09 00:20:33 +0900

    feat: firebase database POST HTTP 요청 전송설정

--- meet-up/src/components/meetups/NewMeetupForm.js
+++ meet-up/src/components/meetups/NewMeetupForm.js
@@ -15,7 +15,7 @@
 import React, { useRef } from 'react';
 import Card from '../../ui/Card';
 import classes from './NewMeetupForm.module.css'
-const NewMeetupForm = () => {
+const NewMeetupForm = (props) => {
     const titleInputRef =useRef();
     const imageInputRef =useRef();
     const descriptionInputRef =useRef();
@@ -36,8 +36,7 @@ const NewMeetupForm = () => {
             description: enterDescription,
         }
 
-        console.log(meetupData);
-
+        props.onAddMeetup(meetupData);
     }
     return (
         <Card>
--- meet-up/src/pages/NewMeetup.js
+++ meet-up/src/pages/NewMeetup.js
@@ -3,6 +3,13 @@
 // ⭐️ 집중하자 집중을
 // -----------------------------------------------------------------------------------------
 // - addMeetup핸들러를 사실은 form파일에서도 진행할수있지만 이미 form파일은 너무크기에 분할하자는 의미로 여기서 사용
+// - http 요청을 보내려면 fetch함수를 사용한다. 최신 브라우저에서 지원하는 자바스크립트 함수.
+//   - 처음엔 요청을 보내는 URL을 입력해야하는데, firebase api는 특이하게 끝에 .json을 입력해야한다.
+//   - 기본적론 get으로 설정이 되어있는데 데이터를 저장해야하므로 post를 보내야한다.
+//   - POST를 보낼때는 저장하려는 데이터를 그 요청에 추가를 해야한다. 이를 body에 담아 보낸다. 이때의 데이터는 대부분 json
+//   - JSON.stringify를 사용하면 객체나 배열 그리고 일반적인 값이 JSON으로 변환된다.
+//   - 마지막으로 원할 경우 header를 추가할수있는데 여기서 주의 할점이 쉼표를 잘 보자. 왜인지는 모르겠으나 쉼표 붙여서 안되던게 됨
+// - 이때 axios 같은 서드파티 패키지를 사용해도 된다. 이도 역시 http요청을 보내기 위해 쓰는 라이브러리.
 // -----------------------------------------------------------------------------------------
 
 import React from 'react';
@@ -11,9 +18,16 @@ import NewMeetupForm from '../components/meetups/NewMeetupForm';
 const NewMeetup = () => {
 
     const addMeetupHandler = (meetupData)=>{
-        
-
+        fetch('https://react-example-started-default-rtdb.firebaseio.com/meetups.json',
+        {
+            method: "POST",
+            body: JSON.stringify(meetupData),
+            headers: {
+                'Content-Type': 'application/json',// 보낼 요청에 메타데이터를 추가 (이는 해당 요청이 json 데이터를 전달하는것을 확실히한다)
+            }
+        });
     }
+    
     return (
         <div>
             <h1>Add New Meetup</h1>
